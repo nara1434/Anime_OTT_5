416f572ad87209ea4fe0410bd1704b5ea7bd211e
prathyusha1217 <prathyushar689@gmail.com>
2025-04-22 06:18:42 +0530

    hgdgw

--- src/index.jsx
+++ src/index.jsx
@@ -16,21 +16,19 @@ import { WishlistProvider } from './Wishlist/WishlistContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <WishlistProvider>
     <BrowserRouter>
+    
       <App />
     </BrowserRouter>
-
-  </React.StrictMode>
-);
-
-
-
-    <WishlistProvider>
-    <App />
     </WishlistProvider>
+
   </React.StrictMode>
 );
+
+
+
+ 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
